ca96e781b32838d31ec9776be536fe51a4d15669
Thomas Conkwright <tmconkwright@gmail.com>
2018-04-23 17:29:09 -0400

    Algorithms: search solutions

---
+++ 02-algorithms/02-search/binary_search_iterative.rb
@@ -0,0 +1,20 @@
+def binary_search(array, value)
+    low = 0
+    high = array.length - 1
+
+    while low <= high
+        mid = (low + high) / 2
+        if array[mid] > value
+            high = mid - 1
+        elsif array[mid] < value
+            low = mid + 1
+        else
+            return mid
+        end
+    end
+    
+    return "Value not found"
+end
+
+list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+puts binary_search(list, 9)
